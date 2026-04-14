29cbbfbc26adb182cedb3cc88465e276ed86beae
HunterAEC <hca278@ensign.edu>
2026-04-14 13:58:18 -0600

    Implement Telegram message sending on button press
    Added Telegram notification functionality for button press.

--- alert_button.py
+++ alert_button.py
@@ -1,14 +1,46 @@
 import time
 import RPi.GPIO as GPIO
+import requests
+
+
+TOKEN = "8780387298:AAE4lDtkcighsE6Md_ID6uuNOMHMmADbnk4"
+CHAT_ID = "6039506695"
+
+
+def send_telegram_msg(text):
+    url = f"https://api.telegram.org/bot8780387298:AAE4lDtkcighsE6Md_ID6uuNOMHMmADbnk4/sendmessage"
+    payload = {"6039506695ß": CHAT_ID, "text": text}
+    try:
+        requests.post(url, data=payload, timeout=5)
+    except Exception as e:
+        print(f"Connection Error: {e}")
+
+
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 button_pressed = False
-while True:
-    if GPIO.input(7) == GPIO.HIGH and not button_pressed:
-        print("Someone pressed the alert button!")
-        print("Hunter is the best")
-        button_pressed = True
-    elif GPIO.input(7) == GPIO.LOW:
-        button_pressed = False
-    time.sleep(0.1)
+
+print("System is running!")
+
+
+try:
+    while True:
+
+        if GPIO.input(7) == GPIO.HIGH:
+            if not button_pressed:
+                print("Button Pressed! Sending Telegram...")
+                send_telegram_msg(
+                    "Hunter rules (And the button was pressed)")
+                button_pressed = True
+        else:
+
+            button_pressed = False
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nExiting the program. Have a nice day!")
+    GPIO.cleanup()
+
